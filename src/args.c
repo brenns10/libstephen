d6badb4de7db743533321ec12cdb2cdcf77b3ce5
Stephen Brennan <stephen.brennan@case.edu>
2014-06-24 15:31:20 -0400

    Switched to Revised BSD license!!  No more petty license restrictions.  It's simple -- if people use my code, they give me credit.  That's it.

--- src/args.c
+++ src/args.c
@@ -1,12 +1,12 @@
-/**
+/***************************************************************************//**
 
-  @file    args.c
+  @file         args.c
 
-  @author  Stephen Brennan
+  @author       Stephen Brennan
 
-  @date    Created Thursday, 31 October 2013
+  @date         Created Thursday, 31 October 2013
 
-  @brief   Contains functions to simplify processing command line args.
+  @brief        Contains functions to simplify processing command line args.
 
   The args library analyzes args as generally as possible.  It recognizes three
   different types.  First is the 'flag' or 'regular flag', which is simply one
@@ -19,8 +19,34 @@
   Finally are bare strings, which are things that aren't passed as parameters or
   as flags.
 
-  @todo    Possibly add support for flags that aren't alphabetical.
-*/
+  @copyright    Copyright (c) 2013-2014, Stephen Brennan.
+  All rights reserved.
+
+  @copyright
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of Stephen Brennan nor the names of his contributors may
+      be used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+  @copyright
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL STEPHEN BRENNAN BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
 
 #include "libstephen.h"
 #include <stdio.h>
--- src/libstephen.h
+++ src/libstephen.h
@@ -1,17 +1,46 @@
-/**
-  @file    libstephen.h
-  
-  @author  Stephen Brennan
+/***************************************************************************//**
 
-  @date    Created Friday, 27 September 2013
+  @file         libstephen.h
 
-  @brief   Public interface of libstephen
+  @author       Stephen Brennan
+
+  @date         Created Friday, 27 September 2013
+
+  @brief        Public interface of libstephen
 
   Libstephen is a C library designed to provide basic data structures and
   operations to supplement the standard C library.  It is mainly a hobby
   programming project, but on completion it should be usable for basing future
   projects on it.
-*/
+
+  @copyright    Copyright (c) 2013-2014, Stephen Brennan.
+  All rights reserved.
+
+  @copyright
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of Stephen Brennan nor the names of his contributors may
+      be used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+  @copyright
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL STEPHEN BRENNAN BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
 
 #ifndef SMB___LIBSTEPHEN_H_
 #define SMB___LIBSTEPHEN_H_
