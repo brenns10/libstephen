54e0873f60384909b1f01dd31471f016705bad37
Stephen Brennan <smb196@case.edu>
2015-03-10 15:13:52 -0400

    Updated Travis GH-Pages build.  Should work now!

--- build_pages.sh
+++ build_pages.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
 # Build GitHub Pages branch automatically.
-
 ## STARTING STATE: Must be in the branch master, with no uncommitted changes.
 ## You've run the tests on release mode, and they run successfully!
+
+# Exit script on first error:
+set -e
+
+# Update git config.
+git config user.name "Travis Builder"
+git config user.email "smb196@case.edu"
 
 # Get the current commit
 COMMIT=$(git rev-parse HEAD)
@@ -17,5 +24,6 @@ make docs
 # Switch to the gh-pages branch, remove the build artifacts.
 git checkout gh-pages
 rm -rf bin obj
+git add .
 git commit -m "[ci skip] Autodoc commit for $COMMIT."
-git push origin gh-pages
+git push "https://$GH_PAGES@github.com/brenns10/libstephen.git"
