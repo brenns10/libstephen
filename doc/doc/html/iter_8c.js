99689054513309778a55fbafabc6eca837717c5d
Stephen Brennan <smb196@case.edu>
2015-04-10 12:59:00 -0400

    Cleaned up the new directories that Travis made accidentally.

--- doc/doc/html/iter_8c.js
+++
@@ -1,4 +0,0 @@
-var iter_8c =
-[
-    [ "iter_print", "iter_8c.html#a56b0eb5936e9f6acde1155b67f358c97", null ]
-];
--- doc/doc/html/search/functions_69.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['iter_5fprint',['iter_print',['../iter_8c.html#a56b0eb5936e9f6acde1155b67f358c97',1,'iter.c']]]
-];
