5ff66d3f9c1b4b8fa1ff733a9229871f4440feee
Travis Builder <smb196@case.edu>
2015-07-10 15:48:14 +0000

    [ci skip] Autodoc commit for cecfae739d42d2569f47a6d26c09799d78522b86.

--- doc/html/charbuf_8c.js
+++ doc/html/charbuf_8c.js
@@ -1,6 +1,7 @@
 var charbuf_8c =
 [
-    [ "cb_append", "charbuf_8c.html#a1548fa018ea62fc308606b209f09afb0", null ],
+    [ "cb_append", "charbuf_8c.html#a96f64ecf859f4c51a1c4bc15462c1bde", null ],
+    [ "cb_concat", "charbuf_8c.html#ad58e21de36a2a1e14eb428a3d758aec4", null ],
     [ "cb_create", "charbuf_8c.html#a31d7843dda9dbec02d949d9a3df9ca1e", null ],
     [ "cb_delete", "charbuf_8c.html#a77192c60e014e95e21b4c5dfc01e928f", null ],
     [ "cb_destroy", "charbuf_8c.html#a58ab43d360c6094a04455fb307f8ecf1", null ],
@@ -8,7 +9,8 @@ var charbuf_8c =
     [ "cb_init", "charbuf_8c.html#a3d2257c33372ae308a644547e2c4a01e", null ],
     [ "cb_printf", "charbuf_8c.html#a0bc356fe4cea7b544e06f8a7d40da499", null ],
     [ "cb_vprintf", "charbuf_8c.html#ab0bfed4c290915b3e2ab7823181f61f2", null ],
-    [ "wcb_append", "charbuf_8c.html#a94a4e4879b84239220b6937b073278f8", null ],
+    [ "wcb_append", "charbuf_8c.html#add5283a52016049800db532ff1b01d36", null ],
+    [ "wcb_concat", "charbuf_8c.html#af982be395693cac5b786e8af7f28437d", null ],
     [ "wcb_create", "charbuf_8c.html#ada972c4d8069d3f4e05d7931b548e8d4", null ],
     [ "wcb_delete", "charbuf_8c.html#afa755dacb080bc26fd441251d4eeb77f", null ],
     [ "wcb_destroy", "charbuf_8c.html#a1aec0ca46db6526b056d38bcbb566ec1", null ],
