20f238c22f59e9ea7e5ac6f17b9eb39d8a22b383
Stephen Brennan <smb196@case.edu>
2015-07-31 12:08:06 -0400

    Moved iter.c declarations into libstephen/list.h.

--- src/iter.c
+++ src/iter.c
@@ -6,7 +6,7 @@
 
   @date         Created Monday,  4 August 2014
 
-  @brief        Libstephen: Iterator Operations
+  @brief        Functions that act on iterators, "libstephen/list.h".
 
   @copyright    Copyright (c) 2013-2015, Stephen Brennan.  Released under the
                 Revised BSD License.  See the LICENSE.txt file for details.
@@ -19,12 +19,6 @@
 #include "libstephen/base.h"
 #include "libstephen/list.h"
 
-/**
-   @brief Prints anything with an iterator.
-   @param it The iterator.  Will be invalidated after this use.
-   @param f The file to print to.
-   @param printer The printer for handling DATA objects.
- */
 void iter_print(smb_iter it, FILE *f, DATA_PRINTER printer)
 {
   DATA d;
