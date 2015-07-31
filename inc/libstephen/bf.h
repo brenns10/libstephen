5cefc15902efd3bc4b396f63d57a10431057970c
Stephen Brennan <smb196@case.edu>
2015-07-31 11:53:30 -0400

    Moved bitfield doc to header.

--- inc/libstephen/bf.h
+++ inc/libstephen/bf.h
@@ -6,7 +6,8 @@
 
   @date         Created Sunday,  3 August 2014
 
-  @brief        Libstephen: Bitfield
+  @brief        A tiny little bit of code that allows for efficient storage and
+                access to large amounts of boolean data.
 
   @copyright    Copyright (c) 2013-2015, Stephen Brennan.  Released under the
                 Revised BSD License.  See the LICENSE.txt file for details.
@@ -35,12 +36,59 @@
 #define SMB_BITFIELD_SIZE(num_bools) ((int)((num_bools) / BIT_PER_CHAR) + \
                                       ((num_bools) % BIT_PER_CHAR == 0 ? 0 : 1))
 
+/**
+   @brief Initialize the memory where a bitfield is contained to all 0's.
+
+   This is public so people can use the function to allocate their own bitfields
+   on function stacks instead of via the heap.  No errors are defined for this
+   function.
+   @param data A pointer to the bitfield.
+   @param num_bools The size of the bitfield, in number of bools (aka bits, not
+     bytes).
+ */
 void bf_init(unsigned char *data, int num_bools);
+/**
+   @brief Allocate a bitfield capable of holding the given number of bools.
+
+   This will be allocated in dynamic memory, and a pointer will be returned.
+   The bf_init function is available for creating bitfields in arbitrary
+   locations.
+   @param num_bools The number of bools to fit in the bit field.
+   @returns A pointer to the bitfield.
+ */
 unsigned char *bf_create(int num_bools);
+/**
+   @brief Delete the bitfield pointed to.  Only do this if you created the
+   bitfield via bf_create().
+   @param data A pointer to the bitfield.
+   @param num_bools The number of bools contained in the bitfield.
+ */
 void bf_delete(unsigned char *data, int num_bools);
+/**
+   @brief Check whether the given bit is set.
+   @param data A pointer to the bitfield.
+   @param index The index of the bit to Check
+   @retval 0 if the bit is not set.
+   @retval Nonzero if the bit is set.
+ */
 int bf_check(unsigned char *data, int index);
+/**
+   @brief Set a bit.
+   @param data A pointer to the bitfield
+   @param index The index of the bit to set.
+ */
 void bf_set(unsigned char *data, int index);
+/**
+   @brief Clear a bit.
+   @param data A pointer to the bitfield.
+   @param index The index of the bit to clear.
+ */
 void bf_clear(unsigned char *data, int index);
+/**
+   @brief Clear a bit.
+   @param data A pointer to the bitfield.
+   @param index The index of the bit to flip.
+ */
 void bf_flip(unsigned char *data, int index);
 
 #endif // LIBSTEPHEN_BF_H
--- src/bitfield.c
+++ src/bitfield.c
@@ -6,8 +6,7 @@
 
   @date         Created Wednesday, 29 January 2014
 
-  @brief        A tiny little bit of code that allows for efficient storage and
-                access to large amounts of boolean data.
+  @brief        Implementation of "libstephen/bf.h".
 
   @copyright    Copyright (c) 2013-2015, Stephen Brennan.  Released under the
                 Revised BSD License.  See the LICENSE.txt file for details.
@@ -20,32 +19,11 @@
 #include "libstephen/base.h"
 #include "libstephen/bf.h"
 
-/**
-   @brief Initialize the memory where a bitfield is contained to all 0's.
-
-   This is public so people can use the function to allocate their own bitfields
-   on function stacks instead of via the heap.  No errors are defined for this
-   function.
-
-   @param data A pointer to the bitfield.
-   @param num_bools The size of the bitfield, in number of bools (aka bits, not
-     bytes).
- */
 void bf_init(unsigned char *data, int num_bools) {
   int size = SMB_BITFIELD_SIZE(num_bools);
   memset(data, 0, size);
 }
 
-/**
-   @brief Allocate a bitfield capable of holding the given number of bools.
-
-   This will be allocated in dynamic memory, and a pointer will be returned.
-   The bf_init function is available for creating bitfields in arbitrary
-   locations.
-
-   @param num_bools The number of bools to fit in the bit field.
-   @returns A pointer to the bitfield.
- */
 unsigned char *bf_create(int num_bools) {
   unsigned char *data;
   int size = SMB_BITFIELD_SIZE(num_bools);
@@ -56,25 +34,10 @@ unsigned char *bf_create(int num_bools) {
   return data;
 }
 
-/**
-   @brief Delete the bitfield pointed to.  Only do this if you created the
-   bitfield via bf_create().
-
-   @param data A pointer to the bitfield.
-   @param num_bools The number of bools contained in the bitfield.
- */
 void bf_delete(unsigned char *data, int num_bools) {
   smb_free(data);
 }
 
-/**
-   @brief Check whether the given bit is set.
-
-   @param data A pointer to the bitfield.
-   @param index The index of the bit to Check
-   @retval 0 if the bit is not set.
-   @retval Nonzero if the bit is set.
- */
 int bf_check(unsigned char *data, int index) {
   int byte_index = (int)(index / BIT_PER_CHAR);
   unsigned char bit_mask = 0x01 << (index % BIT_PER_CHAR);
@@ -82,12 +45,6 @@ int bf_check(unsigned char *data, int index) {
   return data[byte_index] & bit_mask;
 }
 
-/**
-   @brief Set a bit.
-
-   @param data A pointer to the bitfield
-   @param index The index of the bit to set.
- */
 void bf_set(unsigned char *data, int index) {
   int byte_index = (int)(index / BIT_PER_CHAR);
   unsigned char bit_mask = 0x01 << (index % BIT_PER_CHAR);
@@ -95,12 +52,6 @@ void bf_set(unsigned char *data, int index) {
   data[byte_index] |= bit_mask;
 }
 
-/**
-   @brief Clear a bit.
-
-   @param data A pointer to the bitfield.
-   @param index The index of the bit to clear.
- */
 void bf_clear(unsigned char *data, int index) {
   int byte_index = (int)(index / BIT_PER_CHAR);
   unsigned char bit_mask = ~(0x01 << (index % BIT_PER_CHAR));
@@ -108,12 +59,6 @@ void bf_clear(unsigned char *data, int index) {
   data[byte_index] &= bit_mask;
 }
 
-/**
-   @brief Clear a bit.
-
-   @param data A pointer to the bitfield.
-   @param index The index of the bit to flip.
- */
 void bf_flip(unsigned char *data, int index) {
   int byte_index = (int)(index / BIT_PER_CHAR);
   unsigned char bit_mask = 0x01 << (index % BIT_PER_CHAR);
